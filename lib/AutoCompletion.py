d5b44ee9df02158116f74858b778efb49163be4e
tiagoclc <tiagoclc@hotmail.com>
2019-05-07 15:55:32 -0300

    Add files via upload

--- lib/AutoCompletion.py
+++ lib/AutoCompletion.py
@@ -3,30 +3,23 @@
 # Copyright (C) 2015 - Philipp Temminghoff <phil65@kodi.tv>
 # This program is Free Software see LICENSE file for details
 
-import sys
+import xbmcaddon
+import xbmcvfs
 import urllib
 import codecs
 import os
 import time
 import hashlib
-import requests
-import simplejson
-
-import xbmcaddon
-import xbmcvfs
+import urllib2
 import xbmc
-
-PY2 = sys.version_info[0] == 2
-
-HEADERS = {'User-agent': 'Mozilla/5.0'}
+import simplejson
 
 ADDON = xbmcaddon.Addon()
 SETTING = ADDON.getSetting
 ADDON_PATH = os.path.join(os.path.dirname(__file__), "..")
 ADDON_ID = ADDON.getAddonInfo('id')
-ADDON_DATA_PATH = xbmc.translatePath("special://profile/addon_data/%s" % ADDON_ID)
-if PY2:
-    ADDON_DATA_PATH = ADDON_DATA_PATH.decode("utf-8")
+ADDON_DATA_PATH = xbmc.translatePath("special://profile/addon_data/%s" % ADDON_ID).decode("utf-8")
+HEADERS = {'User-agent': 'Mozilla/5.0'}
 
 
 def get_autocomplete_items(search_str, limit=10, provider=None):
@@ -41,21 +34,10 @@ def get_autocomplete_items(search_str, limit=10, provider=None):
         provider = GoogleProvider(limit=limit)
     elif SETTING("autocomplete_provider") == "bing":
         provider = BingProvider(limit=limit)
-    elif SETTING("autocomplete_provider") == "netflix":
-        provider = NetflixProvider(limit=limit)
     else:
         provider = LocalDictProvider(limit=limit)
     provider.limit = limit
     return provider.get_predictions(search_str)
-
-
-def prep_search_str(text):
-    if not isinstance(text, unicode):
-        text = text.decode('utf-8')
-    for char in text:
-        if 1488 <= ord(char) <= 1514:
-            return text[::-1]
-    return text
 
 
 class BaseProvider(object):
@@ -64,17 +46,15 @@ class BaseProvider(object):
         self.limit = kwargs.get("limit", 10)
 
     def get_predictions(self, search_str):
-        if not search_str:
-            return []
-        items = []
-        result = self.fetch_data(search_str)
-        for i, item in enumerate(result):
-            li = {"label": item,
-                  "search_string": prep_search_str(item)}
-            items.append(li)
-            if i > self.limit:
-                break
-        return items
+        pass
+
+    def prep_search_str(self, text):
+        if type(text) != unicode:
+            text = text.decode('utf-8')
+        for chr in text:
+            if ord(chr) >= 1488 and ord(chr) <= 1514:
+                return text[::-1]
+        return text
 
     def get_prediction_listitems(self, search_str):
         for item in self.get_predictions(search_str):
@@ -85,17 +65,32 @@ class BaseProvider(object):
 
 class GoogleProvider(BaseProvider):
 
-    BASE_URL = "http://clients1.google.com/complete/"
-
     def __init__(self, *args, **kwargs):
         super(GoogleProvider, self).__init__(*args, **kwargs)
         self.youtube = kwargs.get("youtube", False)
 
+    def get_predictions(self, search_str):
+        """
+        get dict list with autocomplete labels from google
+        """
+        if not search_str:
+            return []
+        items = []
+        result = self.fetch_data(search_str)
+        for i, item in enumerate(result):
+            li = {"label": item,
+                  "search_string": self.prep_search_str(item)}
+            items.append(li)
+            if i > self.limit:
+                break
+        return items
+
     def fetch_data(self, search_str):
+        base_url = "http://clients1.google.com/complete/"
         url = "search?hl=%s&q=%s&json=t&client=serp" % (SETTING("autocomplete_lang"), urllib.quote_plus(search_str))
         if self.youtube:
             url += "&ds=yt"
-        result = get_JSON_response(url=self.BASE_URL + url,
+        result = get_JSON_response(url=base_url + url,
                                    headers=HEADERS,
                                    folder="Google")
         if not result or len(result) <= 1:
@@ -106,37 +101,35 @@ class GoogleProvider(BaseProvider):
 
 class BingProvider(BaseProvider):
 
-    BASE_URL = "http://api.bing.com/osjson.aspx?"
-
     def __init__(self, *args, **kwargs):
         super(BingProvider, self).__init__(*args, **kwargs)
 
+    def get_predictions(self, search_str):
+        """
+        get dict list with autocomplete labels from bing
+        """
+        if not search_str:
+            return []
+        items = []
+        result = self.fetch_data(search_str)
+        for i, item in enumerate(result):
+            li = {"label": item,
+                  "search_string": self.prep_search_str(item)}
+            items.append(li)
+            if i > self.limit:
+                break
+        return items
+
     def fetch_data(self, search_str):
+        base_url = "http://api.bing.com/osjson.aspx?"
         url = "query=%s" % (urllib.quote_plus(search_str))
-        result = get_JSON_response(url=self.BASE_URL + url,
+        result = get_JSON_response(url=base_url + url,
                                    headers=HEADERS,
                                    folder="Bing")
         if not result:
             return []
         else:
             return result[1]
-
-
-class NetflixProvider(BaseProvider):
-
-    BASE_URL = "http://api-global.netflix.com/desktop/search/autocomplete?"
-
-    def __init__(self, *args, **kwargs):
-        super(NetflixProvider, self).__init__(*args, **kwargs)
-
-    def fetch_data(self, search_str):
-        url = "term=%s" % (urllib.quote_plus(search_str))
-        result = get_JSON_response(url=self.BASE_URL + url,
-                                   headers=HEADERS,
-                                   folder="Bing")
-        if not result or not result["groups"]:
-            return []
-        return [i["title"] for i in result["groups"][0]["items"]]
 
 
 class LocalDictProvider(BaseProvider):
@@ -152,10 +145,9 @@ class LocalDictProvider(BaseProvider):
         k = search_str.rfind(" ")
         if k >= 0:
             search_str = search_str[k + 1:]
-        local = SETTING("autocomplete_lang_local")
-        path = os.path.join(ADDON_PATH, "resources", "data", "common_%s.txt" % (local if local else "en"))
+        path = os.path.join(ADDON_PATH, "resources", "data", "common_%s.txt" % SETTING("autocomplete_lang_local"))
         with codecs.open(path, encoding="utf8") as f:
-            for line in f.readlines():
+            for i, line in enumerate(f.readlines()):
                 if not line.startswith(search_str) or len(line) <= 2:
                     continue
                 li = {"label": line,
@@ -187,7 +179,7 @@ def get_JSON_response(url="", cache_days=7.0, folder=False, headers=False):
             results = simplejson.loads(response)
             log("download %s. time: %f" % (url, time.time() - now))
             save_to_file(results, hashed_url, cache_path)
-        except Exception:
+        except:
             log("Exception: Could not get new JSON data from %s. Tryin to fallback to cache" % url)
             log(response)
             if xbmcvfs.exists(path):
@@ -206,14 +198,16 @@ def get_http(url=None, headers=False):
     """
     succeed = 0
     if not headers:
-        headers = {'User-agent': 'XBMC/16.0 ( phil65@kodi.tv )'}
+        headers = {'User-agent': 'XBMC/14.0 ( phil65@kodi.tv )'}
+    request = urllib2.Request(url)
+    for (key, value) in headers.iteritems():
+        request.add_header(key, value)
     while (succeed < 2) and (not xbmc.abortRequested):
         try:
-            r = requests.get(url, headers=headers)
-            if r.status_code != 200:
-                raise Exception
-            return r.text
-        except Exception:
+            response = urllib2.urlopen(request, timeout=3)
+            data = response.read()
+            return data
+        except:
             log("get_http: could not get data from %s" % url)
             xbmc.sleep(1000)
             succeed += 1
@@ -233,7 +227,7 @@ def read_from_file(path="", raw=False):
                 return f.read()
             else:
                 return simplejson.load(f)
-    except Exception:
+    except:
         log("failed to load textfile: " + path)
         return False
 
